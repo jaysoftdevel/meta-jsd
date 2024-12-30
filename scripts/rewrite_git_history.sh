c90c5b1ec550c1bcb90dfa2d538aa532d5fd43ce
extern.d.junske@eu.denso.com <>
2024-12-30 19:35:45 +0100

    Fix history update script

--- scripts/rewrite_git_history.sh
+++ scripts/rewrite_git_history.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
-git filter-branch -f --env-filter "
-    GIT_AUTHOR_NAME='jaysoftdevel'
-    GIT_AUTHOR_EMAIL='jaysoftdevel@gmail.com'
-    GIT_COMMITTER_NAME='jaysoftdevel'
-    GIT_COMMITTER_EMAIL='jaysoftdevel@gmail.com'
-  " HEAD
+#git filter-branch -f --env-filter "
+#    GIT_AUTHOR_NAME='jaysoftdevel'
+#    GIT_AUTHOR_EMAIL='jaysoftdevel@gmail.com'
+#    GIT_COMMITTER_NAME='jaysoftdevel'
+#    GIT_COMMITTER_EMAIL='jaysoftdevel@gmail.com'
+#  " HEAD
+
+git config --global user.name "jaysoftdevel"
+git config --global user.email "jaysoftdevel@gmail.com"
+
+git rebase  --exec 'git commit --amend --no-edit --reset-author'
