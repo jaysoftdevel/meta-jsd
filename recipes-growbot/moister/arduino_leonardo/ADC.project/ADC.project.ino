a915ac7ef3dc5a76bd5adca82b1a533a34896397
JaySoftDevel <jaysoftdevel@gmail.com>
2025-04-04 15:41:34 +0200

    more changes on moist reader

--- recipes-growbot/moister/arduino_leonardo/ADC.project/ADC.project.ino
+++ recipes-growbot/moister/arduino_leonardo/ADC.project/ADC.project.ino
@@ -1,36 +1,65 @@
 const int AirValue = 520;   //you need to replace this value with Value_1
 const int WaterValue = 260;  //you need to replace this value with Value_2
 int intervals = (AirValue - WaterValue)/3;
-int soilMoistureValue = 0;
+int soilMoistureValue0, soilMoistureValue1 = 0;
+char received_data;
+
+int i=0;
 
 void setup()
 {
   Serial.begin(115200); // open serial port, set the baud rate to 9600 bps
 }
 
+const char* getState(int val){
+  if(val > WaterValue && val < (WaterValue + intervals))
+  {
+    return("Very Wet");
+  }
+  else if(val > (WaterValue + intervals) && val < (AirValue - intervals))
+  {
+    return("Wet");
+  }
+  else if(val < AirValue && val > (AirValue - intervals))
+  {
+    return("Dry");
+  }
+  // fallback
+  return "";
+}
+
 void loop()
 {
-  soilMoistureValue = analogRead(A0);  //put Sensor insert into soil
-  Serial.print(soilMoistureValue);
-  Serial.print(",");
-  //Serial.println((WaterValue + intervals));
-  //Serial.println((AirValue - intervals));
-  
-  if(soilMoistureValue > WaterValue && soilMoistureValue < (WaterValue + intervals))
-  {
-    Serial.print("Very Wet");
+  if(Serial.available()>0)  //check for any data received
+  { 
+    received_data = Serial.read();  //read received data      
+    if(received_data=='0'){
+      soilMoistureValue0 = analogRead(A0);
+      Serial.print(soilMoistureValue0);
+      Serial.print(",");
+      Serial.print(getState(soilMoistureValue0));
+      Serial.write('\r');
+    }
+    else if(received_data=='1')
+    {
+      soilMoistureValue1 = analogRead(A1);
+      Serial.print(soilMoistureValue1);
+      Serial.print(",");
+      Serial.print(getState(soilMoistureValue1));
+      Serial.write('\r');
+    }
+    else if(received_data=='2')
+    {
+      soilMoistureValue0 = analogRead(A0);
+      soilMoistureValue1 = analogRead(A1);
+      Serial.print(soilMoistureValue0);
+      Serial.print(",");
+      Serial.print(getState(soilMoistureValue0));
+      Serial.print(",");
+      Serial.print(soilMoistureValue1);
+      Serial.print(",");
+      Serial.print(getState(soilMoistureValue1));
+      Serial.write('\r');
+    }
   }
-  else if(soilMoistureValue > (WaterValue + intervals) && soilMoistureValue < (AirValue - intervals))
-  {
-    Serial.print("Wet");
-  }
-  else if(soilMoistureValue < AirValue && soilMoistureValue > (AirValue - intervals))
-  {
-    Serial.print("Dry");
-  }
-  else{
-    Serial.print("Optimal moist");
-  }
-  Serial.write('\r');
-  delay(5000);
 }
