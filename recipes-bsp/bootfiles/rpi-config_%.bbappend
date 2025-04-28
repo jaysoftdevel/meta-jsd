4607a0beb32266f66b99cf76ddc60edee3ef5676
extern.d.junske@eu.denso.com <>
2025-04-28 19:22:48 +0200

    update arduino stuff

--- recipes-bsp/bootfiles/rpi-config_%.bbappend
+++ recipes-bsp/bootfiles/rpi-config_%.bbappend
@@ -2,4 +2,6 @@ do_configure:prepend() {
     echo "dtoverlay=uart1-pi5" >> ${S}/config.txt
     echo "dtoverlay=disable-wifi-pi5" >> ${S}/config.txt
     echo "dtoverlay=disable-bt-pi5" >> ${S}/config.txt
+    echo "dtoverlay=gpio-init" >> ${S}/config.txt
+    sed -i 's/#dtparam=i2c_arm=off/dtparam=i2c_arm=off/g'  ${S}/config.txt
 }
--- recipes-growbot/moister/arduino_leonardo/ADC.project/ADC.project.ino
+++ recipes-growbot/moister/arduino_leonardo/ADC.project/ADC.project.ino
@@ -9,94 +9,145 @@ static bool runAtomizer = false;
 static int avVal0[ITERATIONS] = {0};
 static int avVal1[ITERATIONS] = {0};
 
+// Timing control
+unsigned long lastSensorCycleMillis = 0;
+unsigned long lastAtomizerStartMillis = 0;
+unsigned long atomizerDisableMillis = 0;
+
+const unsigned long sensorReadInterval = 10000;  // 10 seconds between sensor cycles
+const unsigned long atomizerRunTime = 7000;     // 7 seconds atomizer on time
+const unsigned long atomizerDisableBeforeReadTime = 3000; // 3 second stabilization
+
+bool atomizerActive = false;
+bool preparingSensorRead = false;
+
 void setup()
 {
-      Serial.begin(115200);
-      Serial1.begin(115200);
-      pinMode(atomizerPin, OUTPUT);
-      digitalWrite(atomizerPin, HIGH); //init not to run, its active low!
+    Serial.begin(115200);
+    Serial1.begin(115200);
+    pinMode(atomizerPin, OUTPUT);
+    digitalWrite(atomizerPin, HIGH); // Atomizer initially OFF (active low)
 }
 
 void loop()
 {
-      int total0 = 0;
-      int total1 = 0;
+    unsigned long currentMillis = millis();
 
-      for (int i = 0; i < ITERATIONS - 1; i++)
-      {
-            avVal0[i+1] = avVal0[i];
-            total0 += avVal0[i+1];
-            avVal1[i+1] = avVal1[i];
-            total1 += avVal1[i+1];
-      }
-      // wait for signal to be stabilized
-      delay(3000);
-      // read back into "old" position 0
-      avVal0[0] = analogRead(A0);
-      avVal1[0] = analogRead(A1);
+    // Handle serial input always
+    checkSerial();
 
-      if (
-          Serial.available() ||
-          Serial1.available()) // check for any data received
-      {
-            char received_data0 = Serial.read();  // read received data
-            char received_data1 = Serial1.read(); // read received data
-            if (
-                received_data0 == '0' ||
-                received_data1 == '0')
-            {
-                  Serial.println(total0/ITERATIONS);
-                  Serial1.println(total0/ITERATIONS);
-            }
-            else if (
-                received_data0 == '1' ||
-                received_data1 == '1')
-            {
-                  Serial.println(total1/ITERATIONS);
-                  Serial1.println(total1/ITERATIONS);
-            }
-            else if (
-                received_data0 == '2' ||
-                received_data1 == '2')
-            {
-                  Serial.print(total0/ITERATIONS);
-                  Serial.print(",");
-                  Serial.print(total1/ITERATIONS);
-                  Serial.print(",");
-                  Serial.println(runAtomizer);
-                  Serial1.print(total0/ITERATIONS);
-                  Serial1.print(",");
-                  Serial1.print(total1/ITERATIONS);
-                  Serial1.print(",");
-                  Serial1.println(runAtomizer);
-            }
-            else if (
-                  received_data0 == '3' ||
-                  received_data1 == '3')
-            {
-                  runAtomizer = true;
-            }
+    // Handle atomizer normal run (outside of sensor read preparation)
+    if (!preparingSensorRead)
+    {
+        if (runAtomizer && !atomizerActive)
+        {
+            digitalWrite(atomizerPin, LOW); // Start atomizer (active low)
+            lastAtomizerStartMillis = currentMillis;
+            atomizerActive = true;
+        }
+        else if (atomizerActive && (currentMillis - lastAtomizerStartMillis >= atomizerRunTime))
+        {
+            digitalWrite(atomizerPin, HIGH); // Turn atomizer OFF
+            atomizerActive = false;
+        }
+    }
 
-            else if (
-                  received_data0 == '4' ||
-                  received_data1 == '4')
-            {
-                  runAtomizer = false;
-            }
-            else
-            {
-                  Serial.print("# ");
-                  Serial.println(received_data0);
-                  Serial1.print("# ");
-                  Serial1.println(received_data1);
-                  return;
-            }
-      }
-      if(runAtomizer==true){
-            // start atomizer
-            digitalWrite(atomizerPin, LOW); // active low!
-      }
-      delay(7000);
-      // always set low, before moist measuring to stabelize
-      digitalWrite(atomizerPin, HIGH); //inactive high!
+    // Handle sensor reading preparation
+    if (!preparingSensorRead && (currentMillis - lastSensorCycleMillis >= sensorReadInterval))
+    {
+        // Time to start disabling atomizer for sensor reading
+        preparingSensorRead = true;
+        atomizerDisableMillis = currentMillis;
+        digitalWrite(atomizerPin, HIGH); // Disable atomizer
+    }
+    else if (preparingSensorRead && (currentMillis - atomizerDisableMillis >= atomizerDisableBeforeReadTime))
+    {
+        // Now stabilized, do the sensor reading
+        performSensorRead();
+        lastSensorCycleMillis = currentMillis;
+        preparingSensorRead = false;
+    }
+}
+
+void checkSerial()
+{
+    if (Serial.available())
+    {
+        char received_data = Serial.read();
+        handleCommand(received_data);
+    }
+    else if (Serial1.available())
+    {
+        char received_data = Serial1.read();
+        handleCommand(received_data);
+    }
+}
+
+void handleCommand(char cmd)
+{
+    int total0 = 0;
+    int total1 = 0;
+    for (int i = 0; i < ITERATIONS; i++)
+    {
+        total0 += avVal0[i];
+        total1 += avVal1[i];
+    }
+    int avg0 = total0 / ITERATIONS;
+    int avg1 = total1 / ITERATIONS;
+
+    if (cmd == '0')
+    {
+        Serial.println(avg0);
+        Serial1.println(avg0);
+    }
+    else if (cmd == '1')
+    {
+        Serial.println(avg1);
+        Serial1.println(avg1);
+    }
+    else if (cmd == '2')
+    {
+        Serial.print(avg0);
+        Serial.print(",");
+        Serial.print(avg1);
+        Serial.print(",");
+        Serial.println(runAtomizer);
+        Serial1.print(avg0);
+        Serial1.print(",");
+        Serial1.print(avg1);
+        Serial1.print(",");
+        Serial1.println(runAtomizer);
+    }
+    else if (cmd == '3')
+    {
+        runAtomizer = true;
+    }
+    else if (cmd == '4')
+    {
+        runAtomizer = false;
+    }
+    else
+    {
+        Serial.print("# ");
+        Serial.println(cmd);
+        Serial1.print("# ");
+        Serial1.println(cmd);
+    }
+}
+
+void performSensorRead()
+{
+    int newVal0 = analogRead(A0);
+    int newVal1 = analogRead(A1);
+
+    // Shift old values
+    for (int i = ITERATIONS - 1; i > 0; i--)
+    {
+        avVal0[i] = avVal0[i - 1];
+        avVal1[i] = avVal1[i - 1];
+    }
+
+    // Store newest reading
+    avVal0[0] = newVal0;
+    avVal1[0] = newVal1;
 }
