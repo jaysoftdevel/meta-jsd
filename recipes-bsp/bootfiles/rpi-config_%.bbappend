1a529130c4c41cfb73ca92c0273e360ac8da3426
JaySoftDevel <jaysoftdevel@gmail.com>
2025-05-18 11:10:30 +0200

    Autostart webcam for photo and activate i2c

--- recipes-bsp/bootfiles/rpi-config_%.bbappend
+++ recipes-bsp/bootfiles/rpi-config_%.bbappend
@@ -1,7 +1,4 @@
 do_configure:prepend() {
-    echo "dtoverlay=uart1-pi5" >> ${S}/config.txt
-    echo "dtoverlay=disable-wifi-pi5" >> ${S}/config.txt
-    echo "dtoverlay=disable-bt-pi5" >> ${S}/config.txt
-    echo "dtoverlay=gpio-init" >> ${S}/config.txt
-    sed -i 's/#dtparam=i2c_arm=off/dtparam=i2c_arm=off/g'  ${S}/config.txt
+    sed -z -i 's/##\n#dtoverlay=act-led,activelow=off/##\ndtoverlay=uart1-pi5\ndtoverlay=disable-wifi-pi5\ndtoverlay=disable-bt-pi5\ndtoverlay=gpio-init\n#dtoverlay=act-led,activelow=off/g' ${S}/config.txt
+    sed -i 's/#dtparam=i2c_arm=off/dtparam=i2c_arm=on\ndtoverlay=i2c1/g' ${S}/config.txt
 }
--- recipes-growbot/moister/files/take-photo.sh
+++ recipes-growbot/moister/files/take-photo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-set -e
+#set -e
 
 OUTPUT_DIR="/var/www/html/webcam_photos"
 VIDEO_OUTPUT_DIR="${OUTPUT_DIR}"
@@ -13,8 +13,15 @@ LAST_INDEX=$(ls "$OUTPUT_DIR"/photo_*.jpg 2>/dev/null | sed -n 's/.*photo_\([0-9
 # Calculate next index
 NEXT_INDEX=$(printf "%04d" $(expr 0 + "$LAST_INDEX" + 1))
 
-# Capture snapshot using wget
-wget -q "http://localhost:8080/?action=snapshot" -O "$OUTPUT_DIR/photo_${NEXT_INDEX}.jpg"
+# Check if camera is running already
+if [ `systemctl is-active mjpg-streamer` == "inactive" ]
+then
+    systemctl start mjpg-streamer
+    wget -q "http://localhost:8080/?action=snapshot" -O "$OUTPUT_DIR/photo_${NEXT_INDEX}.jpg"
+    systemctl stop mjpg-streamer
+else
+    wget -q "http://localhost:8080/?action=snapshot" -O "$OUTPUT_DIR/photo_${NEXT_INDEX}.jpg"
+fi
 
 # Always overwrite timelapse video
 VIDEO_PATH="$VIDEO_OUTPUT_DIR/timelapse_latest.mp4"
